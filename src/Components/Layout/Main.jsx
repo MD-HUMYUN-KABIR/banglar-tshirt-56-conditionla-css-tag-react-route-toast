1e696426c7b87767b8fae13f36c9d7f4d1358995
MD-HUMYUN-KABIR <misbahulhaque0840@gmail.com>
2023-04-07 14:40:27 +0600

    m

--- src/Components/Layout/Main.jsx
+++ src/Components/Layout/Main.jsx
@@ -14,3 +14,13 @@ const Main = () => {
 };
 
 export default Main;
+
+/* 
+interview
+1. how is react routing different from conventional rouating
+2. how do you implement react routing
+3. what is the prupose of context api in react
+4. can you explain how the provider works with the react context api
+5. what is a custom hook
+6. how many way can we implement conditional rendering in react
+*/
